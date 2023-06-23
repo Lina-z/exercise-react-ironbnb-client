ea5d74393d24cc75857ec0dcd100b7213d44ec24
Lina Zerkly <zerkly.lina@gmail.com>
2023-06-23 17:35:51 +0200

    form

--- src/components/CreateApartment.js
+++ src/components/CreateApartment.js
@@ -2,71 +2,79 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
 
+function CreateApartment() {
+  const { apartmentId } = useParams();
 
-function CreateApartment(props) {
-    const { apartmentId } = useParams();
-    
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [details, setDetails] = useState({});
-    const [title, setTitle] = useState("");
-    const [price, setPrice] = useState("");
-    const [img, setImg] = useState("");
+  const [details, setDetails] = useState({});
+  const [title, setTitle] = useState("");
+  const [pricePerDay, setPrice] = useState("");
+  const [img, setImg] = useState("");
 
-    setTitle("");
-    setPrice("");
-    setImg("")
+  setTitle("");
+  setPrice("");
+  setImg("");
 
-    const useHandleSubmit = (e) => {
-        e.preventDefault();
-        const newApartment = {
-          title: title,
-          price: price,
-          img: img,
-        };
-        props.addApartment(newApartment)
+  const useHandleSubmit = (e) => {
+    e.preventDefault();
+    const newApartment = {
+      title: title,
+      price: pricePerDay,
+      img: img,
+    };
+    addApartment(newApartment);
+  };
 
-    useEffect(() => {
-        axios.post(`${process.env.REACT_APP_API_URL}/apartments/${apartmentId}`, setDetails)
-            .then(response => {
-                setDetails(response.data);
-            })
-            .catch(e => console.log(e))
-    }, []);
+  const createApartment = () => {
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/apartments`, newApartment)
+      .then((response) => {
+       // props.callbackToUpdateList();
+        navigate("/apartments");
+      })
+      .catch((e) => console.log("Error creating appartment...", e));
+  };
 
-    const createApartment = () => {
-        axios.create(`${process.env.REACT_APP_API_URL}/apartments/${apartmentId}`)
-            .then( (response) => {
-                props.callbackToUpdateList(); 
-                navigate("/"); 
-            })
-            .catch( e => console.log("Error creating appartment...", e));
-    }
-
-    return (
-        <div className="box">
-
-        <form onSubmit={useHandleSubmit}>
+  return (
+    <div className="box">
+      <form onSubmit={useHandleSubmit}>
         <span>Add Apartment</span>
-  
+
         <label>Name</label>
-        <span value={title} type="text" onChange={(e) => {setTitle(e.target.value)}} />
-  
+        <span
+          value={title}
+          type="text"
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+
         <label>Image</label>
-        <span value={price} type="text" onChange={(e) => {setPrice(e.target.value)}} />
-  
+        <span
+          value={pricePerDay}
+          type="text"
+          onChange={(e) => {
+            setPrice(e.target.value);
+          }}
+        />
+
         <label>Image</label>
-        <span value={img} type="text" onChange={(e) => {setImg(e.target.value)}} />
-  
+        <span
+          value={img}
+          type="text"
+          onChange={(e) => {
+            setImg(e.target.value);
+          }}
+        />
+
         <button onClick={createApartment}>Create this apartment</button>
 
-            <p>
+        {/*         <p>
                 <Link to="/">Back</Link>
-            </p>
-      
+            </p> */}
       </form>
-      </div>);
-    
-}
+    </div>
+  );
 }
 export default CreateApartment;
